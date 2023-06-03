86bc2f7f3f5ebc0ecfd8fd989c8b5722ecc84557
buihien0109 <buihien01091997@gmail.com>
2023-06-03 12:27:59 +0700

    update

--- day_26/jpa-relationship-other/src/main/java/com/example/jparelationship/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jparelationship.repository;
-
-import com.example.jparelationship.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
--- day_26/jpa-relationship-other/src/main/java/com/example/jparelationship/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jparelationship.repository;
-
-import com.example.jparelationship.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
