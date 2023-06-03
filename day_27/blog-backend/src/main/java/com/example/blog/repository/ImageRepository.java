37391a6915d12c3666f15475f782ced45e077b82
buihien0109 <buihien01091997@gmail.com>
2023-06-03 12:29:07 +0700

    update

--- day_27/blog-backend/src/main/java/com/example/blog/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog.repository;
-
-import com.example.blog.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-}
